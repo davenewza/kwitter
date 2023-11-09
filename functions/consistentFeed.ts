fdd7b28029c1a32c16a4a683df5f25526974df10
Dave New <sanodn@gmail.com>
2023-11-09 12:42:54 +0200

    wip

--- functions/consistentFeed.ts
+++ functions/consistentFeed.ts
@@ -1,7 +1,7 @@
 import { ConsistentFeed, Feed, models } from '@teamkeel/sdk';
 
-export default ConsistentFeed(async (_, inputs) => {
-    const follows = await models.follow.findMany({ where: { followerId: inputs.id }});
+export default ConsistentFeed(async (ctx, _) => {
+    const follows = await models.follow.findMany({ where: { followerId: ctx.identity!.id }});
 
     const feed:Feed[] = [];
 
@@ -10,7 +10,7 @@ export default ConsistentFeed(async (_, inputs) => {
         const tweets = await models.tweet.findMany({ where: { accountId: followee!.id }});
 
         for (const t of tweets) {
-            feed.push( { id: "1", username: followee!.username, accountId: inputs.id, content: t!.content, createdAt: t.createdAt, updatedAt: t.createdAt, tweetedAt: t.createdAt });
+            feed.push( { id: "1", username: followee!.username, accountId: ctx.identity!.id, content: t!.content, createdAt: t.createdAt, updatedAt: t.createdAt, tweetedAt: t.createdAt });
         }
     }
 
