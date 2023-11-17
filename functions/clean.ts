32ba18f9a87d9ef4bb3752bff26a349394b94240
Dave New <sanodn@gmail.com>
2023-11-17 05:44:26 +0200

    clean fix

--- functions/clean.ts
+++ functions/clean.ts
@@ -1,12 +1,11 @@
 import { Clean, models } from '@teamkeel/sdk';
 export default Clean(async (ctx, inputs) => {
 
-    const accounts = await models.identity.findMany();
+    const identities = await models.identity.findMany();
 
-    accounts.forEach(async function(acc) {
-        await  models.identity.delete({ id: acc.id });
-        console.log(acc.id);
-    });
+    for (const identity of identities) {
+        await  models.identity.delete({ id: identity.id });
+    };
 
-    return {count: accounts.length };
+    return {count: identities.length };
 })
